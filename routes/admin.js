6e38310376c68837291102b5c1e454413002b488
Sachin Sharma <sachin.kumarsharam09@gmail.com>
2023-02-14 16:26:51 +0530

    Shop-details and Admin-addproduct page created

--- routes/admin.js
+++ routes/admin.js
@@ -5,6 +5,7 @@ const router = express.Router();
 const VIEWS_NAME = "views";
 const ADMIN_PRODUCTS_FILE = "admin-products";
 const ADMIN_FOLDER = "admin";
+const ADMIN_ADDPRODUCT_FILE = "admin-addproduct";
 
 router.get("/products", (req, res, next) => {
     res.render(
@@ -14,6 +15,18 @@ router.get("/products", (req, res, next) => {
             VIEWS_NAME,
             ADMIN_FOLDER,
             ADMIN_PRODUCTS_FILE
+        )
+    );
+});
+
+router.get("/addproduct", (req, res, next) => {
+    res.render(
+        path.join(
+            __dirname,
+            "..",
+            VIEWS_NAME,
+            ADMIN_FOLDER,
+            ADMIN_ADDPRODUCT_FILE
         )
     );
 });
--- routes/shop.js
+++ routes/shop.js
@@ -4,7 +4,14 @@ const router = express.Router();
 
 const VIEWS_NAME = "views";
 const SHOP_PRODUCTS_FILE = "shop-products";
+const SHOP_DETAILS_FILE = "shop-details";
 const SHOP_FOLDER = "shop";
+
+router.get("/", (req, res, next) => {
+    res.render(
+        path.join(__dirname, "..", VIEWS_NAME, SHOP_FOLDER, SHOP_DETAILS_FILE)
+    );
+});
 
 router.get("/products", (req, res, next) => {
     res.render(
